bfc3d1d8014665a96ea9dd20c1331e4b5d7df0ba
Rainey <changjiu1391@126.com>
2022-09-19 18:39:33 +0800

    revoke reading file dir

--- test/index.js
+++ test/index.js
@@ -1,16 +1,7 @@
-const fs = require('fs')
-const path = require('path')
-let dirPath = path.resolve(__dirname, '../lib');
-let files = fs.readdirSync(dirPath); // 该文件夹下的所有文件名称 (文件夹 + 文件)
-
-let modulesNames = files.filter(filename => filename != 'index.js')
-
-function dealFileName(fileName) {
-    let arr = fileName.substring(0, fileName.length - 3).split('-')
-    if (arr.length > 1) {
-        arr[1] = arr[1].replace(/^./, arr[1][0].toUpperCase())
-    }
-    return arr.join('')
+module.exports = {
+    dataProcess: require('../lib/data-process'),
+    flowPath: require('../lib/flow-path.js'),
+    math: require('../lib/math.js'),
+    movement: require('../lib/movement.js'),
+    page: require('../lib/page.js'),
 }
-modulesNames.forEach(fileName => { exports[dealFileName(fileName)] = require(path.resolve(__dirname, `../lib/${fileName}`)) })
-
